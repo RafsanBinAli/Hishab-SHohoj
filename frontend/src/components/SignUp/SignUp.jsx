6ef9f0f832139849bd8a38e5c62e93f7b1f97296
aditta-saikat <u1904121@student.cuet.ac.bd>
2024-07-12 00:21:30 +0600

    sign up added

--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -1,26 +1,19 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useState } from "react";
 import "./SignUp.css";
 
 const SignUp = () => {
-  // Define toggle function
-  const toggle = useCallback(() => {
-    let container = document.getElementById("container");
-    container.classList.toggle("sign-in");
-    container.classList.toggle("sign-up");
-  }, []);
+  const [isSignUp, setIsSignUp] = useState(true);
 
-  useEffect(() => {
-    const container = document.getElementById("container");
-    setTimeout(() => {
-      container.classList.add("sign-in");
-    }, 200);
-  }, []);
+  const toggle = () => {
+    setIsSignUp(!isSignUp);
+  };
 
   return (
-    <div id="container" className="container-sign sign-up">
-      {/* FORM SECTION */}
+    <div
+      id="container-sign"
+      className={`container-sign ${isSignUp ? "sign-up" : "sign-in"}`}
+    >
       <div className="row">
-        {/* SIGN UP */}
         <div className="col align-items-center flex-col sign-up">
           <div className="form-wrapper align-items-center">
             <div className="form sign-up">
@@ -50,8 +43,6 @@ const SignUp = () => {
             </div>
           </div>
         </div>
-        {/* END SIGN UP */}
-        {/* SIGN IN */}
         <div className="col align-items-center flex-col sign-in">
           <div className="form-wrapper align-items-center">
             <div className="form sign-in">
@@ -76,29 +67,21 @@ const SignUp = () => {
             </div>
           </div>
         </div>
-        {/* END SIGN IN */}
       </div>
-      {/* END FORM SECTION */}
-      {/* CONTENT SECTION */}
       <div className="row content-row">
-        {/* SIGN IN CONTENT */}
         <div className="col align-items-center flex-col">
           <div className="text sign-in">
             <h2>Welcome</h2>
           </div>
           <div className="img sign-in"></div>
         </div>
-        {/* END SIGN IN CONTENT */}
-        {/* SIGN UP CONTENT */}
         <div className="col align-items-center flex-col">
           <div className="img sign-up"></div>
           <div className="text sign-up">
             <h2>Join with us</h2>
           </div>
         </div>
-        {/* END SIGN UP CONTENT */}
       </div>
-      {/* END CONTENT SECTION */}
     </div>
   );
 };
