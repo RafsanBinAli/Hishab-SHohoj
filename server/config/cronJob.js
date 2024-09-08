86c305b58a535a6592ea54b48d9ac93076956f01
Rafsan Bin Ali <154937557+RafsanBinAli@users.noreply.github.com>
2024-09-08 12:11:18 +0600

    Update cronJob.js

--- server/config/cronJob.js
+++ server/config/cronJob.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const baseUrl = process.env.BASE_URL || 'http://localhost:4000';
 
 cron.schedule(
-  '10 12 * * *',
+  '05 0 * * *',
   async () => {
     try {
       const response = await axios.post(`${baseUrl}/transaction/create-daily`);
