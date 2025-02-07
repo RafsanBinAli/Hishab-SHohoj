fb765e20127e92a698d01fd06fb3e603b0166619
RafsanBinAli <mr.chese.2@gmail.com>
2025-02-08 03:38:08 +0600

    removed views and unnecessary things from codebase

--- server/app.js
+++ server/app.js
@@ -1,29 +1,32 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose"); 
-var cors = require("cors");
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var transactionRouter = require("./routes/transaction")
-var debtRouter =require("./routes/debt")
-var bankRouter =require("./routes/bank")
-const dotenv = require('dotenv').config()
-require('./config/cronJob'); 
-const connectDB = require("./config/Database")
-var app = express();
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const http = require('http');
+require('dotenv').config();
+require('./config/cronJob');
 
-const MONGODB_URI = process.env.MONGODB_URI;
+// Import routes
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const transactionRouter = require("./routes/transaction");
+const debtRouter = require("./routes/debt");
+const bankRouter = require("./routes/bank");
 
-connectDB();
+// Database connection
+const connectDB = require("./config/Database");
 
+// Initialize express app
+const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// Connect to MongoDB
+connectDB().catch(err => {
+  console.error('Failed to connect to MongoDB:', err);
+});
 
+// Middleware setup
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,26 +34,38 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
+// Routes
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/transaction",transactionRouter)
-app.use("/debt",debtRouter)
-app.use("/bank",bankRouter)
+app.use("/transaction", transactionRouter);
+app.use("/debt", debtRouter);
+app.use("/bank", bankRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+// 404 handler
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+// Error handler
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {}
+  });
+});
+
+// Server setup
+const port = process.env.PORT || '4000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
