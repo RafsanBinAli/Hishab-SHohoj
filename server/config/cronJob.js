ec5673fd31413a3ed4d9a812359ccb2153f14743
Rafsan Bin Ali <mr.chese.2@gmail.com>
2025-04-07 00:05:26 +0600

    changed time of cron job

--- server/config/cronJob.js
+++ server/config/cronJob.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const baseUrl = process.env.BACKEND_URL;
 
 cron.schedule(
-  "05 0 * * *", 
+  "07 0 * * *", 
   async () => {
     try {
       const response = await axios.post(`${baseUrl}/transaction/create-daily`);
